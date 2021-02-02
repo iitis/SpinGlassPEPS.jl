095f44e8972ed791ab7cfd87d9fb64d8e860a75a
kdomino <kdomino@iitis.pl>
2021-02-02 09:14:38 +0100

    correction

--- test/runtests.jl
+++ test/runtests.jl
@@ -36,7 +36,7 @@ push!(my_tests,
     "PEPS.jl",
     "indexing.jl",
     "notation_tests.jl",
-    "peps_tests.jl"
+    "peps_tests.jl",
     "mps_tests.jl",
     "tests_full_graph.jl",
     "tests_on_data.jl"
